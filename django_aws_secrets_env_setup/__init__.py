8f387e608481e454ea5c1d1fc0841a1e712adae7
Josh Dennis <josh@joshdennis.net>
2021-06-02 15:48:50 -0500

    exception. handling updates

--- django_aws_secrets_env_setup/__init__.py
+++ django_aws_secrets_env_setup/__init__.py
@@ -11,16 +11,11 @@ def __check_kwargs_then_environ__(kwarg_key, environ_key, **kwargs):
         try:
             return os.environ[environ_key]
         except:
-            print(f'{kwarg_key} was not found in kwargs and {environ_key} was not found in environment variables')
-            raise
+            raise Exception(f'{kwarg_key} was not found in kwargs and {environ_key} was not found in environment variables')
 
 def __get_secrets(default_region_name, **kwargs):
-    secrets_name_env_name = kwargs.get('secrets_name_env_name', 'SECRETS_NAME')
-    secrets_name = __check_kwargs_then_environ__('secrets_name', secrets_name_env_name, **kwargs)
-
-    region_name_env_name = kwargs.get('region_name_env_name', 'SECRETS_REGION_NAME')
-    region_name = os.environ.get(region_name_env_name, default_region_name)
-
+    secrets_name = __check_kwargs_then_environ__('secrets_name', 'SECRETS_NAME', **kwargs)
+    region_name = os.environ.get("SECRETS_REGION_NAME", default_region_name)
     session = boto3.session.Session()
     try:
         client = session.client(
@@ -34,9 +29,7 @@ def __get_secrets(default_region_name, **kwargs):
             aws_access_key_id_env_name = kwargs.get('aws_access_key_id_env_name', 'AWS_ACCESS_KEY_ID')
             aws_access_key_id = __check_kwargs_then_environ__('aws_access_key_id', aws_access_key_id_env_name, **kwargs)
         finally:
-            aws_secret_access_key_env_name = kwargs.get('aws_secret_access_key_env_name', 'AWS_SECRET_ACCESS_KEY')
-            aws_secret_access_key = __check_kwargs_then_environ__('aws_secret_access_key', aws_secret_access_key_env_name, **kwargs)
-
+            aws_secret_access_key = __check_kwargs_then_environ__('aws_secret_access_key', 'AWS_SECRET_ACCESS_KEY', **kwargs)
         try:
             client = session.client(
                 service_name=kwargs.get('service_name', 'secretsmanager'),
@@ -45,29 +38,23 @@ def __get_secrets(default_region_name, **kwargs):
                 aws_secret_access_key=aws_secret_access_key
             )
         except:
-            print("There was an error with the AWS access key and/or its ID.")
-            print("Either the values passed to get_secrets were invalid, or the environment variables are configured incorrectly.")
-            raise
-
+            raise Exception("There was an error with the AWS access key and/or its ID.  Either the values passed to get_secrets were invalid, or the environment variables are configured incorrectly.")
     try:
         get_secret_value_response = client.get_secret_value(SecretId=secrets_name)
     except ClientError as e:
-        print("There was an error getting the secret from the secret manager.")
         if e.response['Error']['Code'] == 'DecryptionFailureException':
-            print("Secrets Manager can't decrypt the protected secret text using the provided KMS key.")
+            raise Exception("Secrets Manager can't decrypt the protected secret text using the provided KMS key.")
         elif e.response['Error']['Code'] == 'InternalServiceErrorException':
-            print("An error occurred on the server side.")
+            raise Exception("An error occurred on the server side.")
         elif e.response['Error']['Code'] == 'InvalidParameterException':
-            print("You provided an invalid value for a parameter.")
+            raise Exception("You provided an invalid value for a parameter.")
         elif e.response['Error']['Code'] == 'InvalidRequestException':
-            print("You provided a parameter value that is not valid for the current state of the resource.")
+            raise Exception("You provided a parameter value that is not valid for the current state of the resource.")
         elif e.response['Error']['Code'] == 'ResourceNotFoundException':
-            print("We can't find the resource that you asked for.")
-        raise e
+            raise Exception("We can't find the resource that you asked for.")
+        raise Exception("There was an error getting the secret from the secret manager.")
     except:
-        print("There was an unknown error getting the secret from the secret manager.")
-        print("This is likely due to an error with secrets_name, aws_access_key_id, or aws_secret_access_key.")
-        raise
+        raise Exception("There was an unknown error getting the secret from the secret manager.  This is likely due to an error with secrets_name, aws_access_key_id, or aws_secret_access_key.")
 
     if 'SecretString' in get_secret_value_response:
         secrets = get_secret_value_response['SecretString']
@@ -75,12 +62,14 @@ def __get_secrets(default_region_name, **kwargs):
         secrets = base64.b64decode(get_secret_value_response['SecretBinary'])
     return json.loads(secrets)
 
-
-def set_env_variables(default_region_name, **kwargs):
+def set_env_variables(default_region_name, stop_on_failure=False, **kwargs):
     try:
         secrets = __get_secrets(default_region_name, **kwargs)
-
         for k,v in secrets.items():
             os.environ[k] = str(v)
-    except:
-        pass
+    except Exception as e:
+        if stop_on_failure:
+            raise Exception(e)
+        else:
+            print(e)
+            pass

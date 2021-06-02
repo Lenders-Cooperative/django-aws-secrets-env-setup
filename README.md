bbb9bb0c10462e3fdac34010593025af709445e1
Levi Anderson <lax93@mst.edu>
2021-06-02 14:42:09 -0500

    Add more parameters & logging for bad parameters/environment variables

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # django-aws-secrets-env-setup
 This is a helper function to set up environment variables from AWS secrets manager.
-The main function to use is `set_env_variables(secret_name, default_region_name)`.
-`secret_name` is the name of the secret to get from the manager.
-`default_region_name` is the region that should be used if `os.environ["SECRETS_REGION_NAME"]` is not set.
+The main function to use is `set_env_variables(default_region_name, **kwargs)`.
+In addition to `default_region_name`, there are several kwargs that can be passed.
+#### Parameters
+- If the `os.environ['SECRETS_REGION_NAME']` is not set, the `default_region_name` will be used as the `region_name` instead.
+- `secrets_name` is the name of the secret to pull secrets from. Default: `os.environ['SECRETS_NAME']'`
+- `aws_access_key_id` is the ID of the AWS access key to be used. Default: `os.environ['AWS_ACCESS_KEY_ID']'`
+- `aws_secret_access_key` is the AWS key used to access the secrets. Default: `os.environ['AWS_SECRET_ACCESS_KEY']'`
+- `service_name` is the service name of the secrets manager to connect to. Default: `'secretsmanager'`
--- django_aws_secrets_env_setup/__init__.py
+++ django_aws_secrets_env_setup/__init__.py
@@ -1,14 +1,22 @@
 import boto3
+from botocore.exceptions import ClientError
 import os
 import base64
 import json
 
+def __check_kwargs_then_environ__(kwarg_key, environ_key, **kwargs):
+    if kwarg_key in kwargs:
+        return kwargs[kwarg_key]
+    else:
+        try:
+            return os.environ[environ_key]
+        except:
+            print(f'{kwarg_key} was not found in kwargs and {environ_key} was not found in environment variables')
+            raise
 
-def __get_secrets(secret_name, default_region_name, **kwargs):
-    try:
-        region_name = os.environ["SECRETS_REGION_NAME"]
-    except:
-        region_name = default_region_name
+def __get_secrets(default_region_name, **kwargs):
+    secrets_name = __check_kwargs_then_environ__('secrets_name', 'SECRETS_NAME', **kwargs)
+    region_name = os.environ.get("SECRETS_REGION_NAME", default_region_name)
 
     session = boto3.session.Session()
     try:
@@ -17,14 +25,45 @@ def __get_secrets(secret_name, default_region_name, **kwargs):
             region_name=region_name
         )
     except:
-        client = session.client(
-            service_name=kwargs.get('service_name', 'secretsmanager'),
-            region_name=region_name,
-            aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
-            aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"]
-        )
+        # This uses a try/finally to make sure the key's existence is always checked regardless of the id's existence.
+        # It's intended to ensure an error with the id doesn't cover an error with the key.
+        try:
+            aws_access_key_id = __check_kwargs_then_environ__('aws_access_key_id', 'AWS_ACCESS_KEY_ID', **kwargs)
+        finally:
+            aws_secret_access_key = __check_kwargs_then_environ__('aws_secret_access_key', 'AWS_SECRET_ACCESS_KEY', **kwargs)
 
-    get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+        try:
+            client = session.client(
+                service_name=kwargs.get('service_name', 'secretsmanager'),
+                region_name=region_name,
+                aws_access_key_id=aws_access_key_id,
+                aws_secret_access_key=aws_secret_access_key
+            )
+        except:
+            print("There was an error with the AWS access key and/or its ID.")
+            print("Either the values passed to get_secrets were invalid, or the environment variables are configured incorrectly.")
+            raise
+
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secrets_name)
+    except ClientError as e:
+        print("There was an error getting the secret from the secret manager.")
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            print("Secrets Manager can't decrypt the protected secret text using the provided KMS key.")
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            print("An error occurred on the server side.")
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            print("You provided an invalid value for a parameter.")
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            print("You provided a parameter value that is not valid for the current state of the resource.")
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            print("We can't find the resource that you asked for.")
+        raise e
+    except:
+        print("There was an unknown error getting the secret from the secret manager.")
+        print("This is likely due to an error with secrets_name, aws_access_key_id, or aws_secret_access_key.")
+        raise
+
     if 'SecretString' in get_secret_value_response:
         secrets = get_secret_value_response['SecretString']
     else:
@@ -32,9 +71,9 @@ def __get_secrets(secret_name, default_region_name, **kwargs):
     return json.loads(secrets)
 
 
-def set_env_variables(secret_name, default_region_name, **kwargs):
+def set_env_variables(default_region_name, **kwargs):
     try:
-        secrets = __get_secrets(secret_name, default_region_name, **kwargs)
+        secrets = __get_secrets(default_region_name, **kwargs)
 
         for k,v in secrets.items():
             os.environ[k] = str(v)
